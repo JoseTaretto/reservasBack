922942ab067f1f5e797a49bc12713748a786a1ba
TeemoPseudoDev <jose.martinez.taretto@gmail.com>
2024-09-03 02:28:49 -0300

    ruteo y metodos locales

---
+++ data.js
@@ -0,0 +1,28 @@
+negocios = [
+    {
+        _id:1,
+        name:"Calzados el viejo amaya",
+        datetime:"lun a vie de 9:00 a 18:00",
+        img:'dfdfdf'
+    },
+    {
+        _id:2,
+        name:"Panaderia Joakoko's",
+        datetime:"lun a vie de 9:00 a 18:00",
+        img:'dfdfdf'
+    },
+    {
+        _id:3,
+        name:"El gato ffeliz",
+        datetime:"lun a vie de 9:00 a 18:00",
+        img:'dfdfdf'
+    },
+    {
+        _id:4,
+        name:"Pizzeria los hijos de puta",
+        datetime:"lun a vie de 9:00 a 18:00",
+        img:'dfdfdf'
+    }
+];
+
+module.exports = { negocios };
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express')
+const express = require('express');
+
+const { negocios } = require('./data'); // 'data' es el nombre del archivo donde está exportado
 
 
 const app = express()
 const PORT = 3000;
 
+const routerNegocios = express.Router();
 
-app.get('/api/negocios', function (req, res) {
-  res.send('1')
+app.use('/api/negocios', routerNegocios)
+
+routerNegocios.get('/', function (req, res) {
+  console.log(typeof negocios)
+  res.send(negocios)
 })
+
+routerNegocios.get('/:id', function (req, res) {
+  const negocioId = parseInt(req.params.id);
+  const negocio = negocios.find(n => n._id === negocioId);
+
+  if (negocio) {
+    res.send(negocio);
+  } else {
+    res.status(404).send({ message: 'Negocio no encontrado' });
+  }
+});
 
 app.listen(PORT)
